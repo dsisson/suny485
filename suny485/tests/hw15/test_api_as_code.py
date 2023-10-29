4468a08109275e9554f97c3c104fa0d6aa234fce
derek <philosophe@gmail.com>
2023-10-29 13:39:31 -0400

    homework 15
    projects/hw15/password_utils.py
    + copied application code from hw14
    
    projects/hw15/api.py
    + added get_password_strength_by_post()
    
    tests/hw15/test_api_as_code.py
    + added client_get_endpoint & client_post_endpoint fixtures
    + added TestApiGetThroughCode and TestApiPostThroughCode classes
    
    tests/hw15/test_api_as_live.py
    + added TestApiGetThroughHttp and TestApiPostThroughHttp classes

---
+++ suny485/tests/hw15/test_api_as_code.py
@@ -0,0 +1,262 @@
+import json
+import pytest
+import requests
+from requests.exceptions import MissingSchema
+
+from suny485.projects.hw15.api import app
+
+"""
+testable things for API GET endpoint
++ input
+    + key
+        + string
+            + must be 'password'
+            + else returns 500
+    + value
+        + string
+            + any string
+            + length of string
++ logic
+    + computes strength for good key/value pair
+    + http truncates url at `#`
+    + http truncates url at `&`
+    + empty value string throws DivByZero
++ output
+    + json payload
+        + key/value
+            + "password"
+                original string
+            + "strength"
+                either "good" or "bad"
+
+testable things for API GET endpoint
+
+"""
+
+
+@pytest.fixture
+def client_get_endpoint():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture
+def client_post_endpoint():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+class TestApiGetThroughCode(object):
+
+    @pytest.mark.parametrize(
+        'good_strength_get',
+        [
+            '_@4-%@o_rds',
+            '$$$$$$$ord'
+        ]
+    )
+    def test_api_good_password(self, client_get_endpoint, good_strength_get):
+        # disambiguate
+        password = good_strength_get  # flake8 will say this is bad; ignore that!
+        expected_strength = 'good'
+
+        response = client_get_endpoint.get(f"/get_strength?password={password}")
+        data = json.loads(response.data.decode())
+
+        assert response.status_code == 200
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_strength_get',
+        [
+            '_@4-%o_rds',
+            '$password',
+            'x' * 100,
+            'x' * 1000,
+            'x' * 10000,
+        ], ids=[
+            '_@4-%o_rds',
+            '$password',
+            '100chars',
+            '1000chars',
+            '10000chars',
+        ]
+    )
+    def test_api_bad_password(self, client_get_endpoint, bad_strength_get):
+        # disambiguate
+        password = bad_strength_get  # flake8 will say this is bad; ignore that!
+        expected_strength = 'bad'
+
+        response = client_get_endpoint.get(f"/get_strength?password={password}")
+        data = json.loads(response.data.decode())
+
+        assert response.status_code == 200
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'errors_get',
+        [
+            '_@4-#%o_rds',
+            'pass#word',
+            'pa&&word',
+        ]
+    )
+    def test_query_string_truncation(self, client_get_endpoint, errors_get):
+        # a `#` in the query string will be truncated
+        # disambiguate
+        password = errors_get  # flake8 will say this is bad; ignore that!
+
+        response = client_get_endpoint.get(f"/get_strength?password={password}")
+        data = json.loads(response.data.decode())
+
+        assert response.status_code == 200
+        print(f"data: {data}")
+        # the char `#` is not allowed in this url; anything after the `#`
+        # is stripped from the request
+        assert data.get('password') != password
+        assert '#' not in data.get('password')
+
+    @pytest.mark.parametrize(
+        'errors_get',
+        [
+            '_@4-#%o_rds',
+            'pass#word',
+            'pa&&word',
+        ]
+    )
+    def test_query_string_no_truncation_as_params(self, client_get_endpoint, errors_get):
+        # a `#` in the query string will be truncated
+        # disambiguate
+        password = errors_get  # flake8 will say this is bad; ignore that!
+        # assemb;le params
+        params = {'password': password}
+
+        response = client_get_endpoint.get(f"/get_strength", query_string=params)
+        #print(f"url --> {response.url}")
+        data = json.loads(response.data.decode())
+
+        assert response.status_code == 200
+        print(f"data: {data}")
+        # the char `#` is not allowed in this url; anything after the `#`
+        # is stripped from the request
+        assert data.get('password') == password
+
+    @pytest.mark.parametrize(
+        'div_by_zero_get',
+        [
+            '#password',
+            ''
+        ]
+    )
+    def test_api_error(self, client_get_endpoint, div_by_zero_get):
+        # disambiguate
+        password = div_by_zero_get  # flake8 will say this is bad; ignore that!
+
+        with pytest.raises(ZeroDivisionError):
+            client_get_endpoint.get(f"/get_strength?password={password}")
+
+
+
+class TestApiPostThroughCode(object):
+
+    @pytest.mark.parametrize(
+        'good_strength_post',
+        [
+            '_@4-%@o_rds',
+            '$$$$$$$ord',
+            '_@4-#%o_rds',  # was get error
+        ]
+    )
+    def test_api_good_password(self, client_post_endpoint, good_strength_post):
+        # disambiguate
+        password = good_strength_post  # flake8 will say this is bad; ignore that!
+        expected_strength = 'good'
+
+        # assemble the payload
+        data = {'password': password}
+
+        response = client_post_endpoint.post('/get_strength', json=data)
+        assert response.status_code == 200
+        res_data = json.loads(response.data.decode())
+
+        print(f"res_data: {res_data}")
+        assert res_data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_strength_post',
+        [
+            '_@4-%o_rds',
+            '$password',
+            'pass#word',  # was get error
+            'pa&&word',   # was get error
+            'x' * 100,
+            'x' * 1000,
+            'x' * 10000,
+        ], ids=[
+            '_@4-%o_rds',
+            '$password',
+            'pass#word',
+            'pa&&word',
+            '100chars',
+            '1000chars',
+            '10000chars',
+        ]
+    )
+    def test_api_bad_password(self, client_post_endpoint, bad_strength_post):
+        # disambiguate
+        password = bad_strength_post  # flake8 will say this is bad; ignore that!
+        expected_strength = 'bad'
+
+        # assemble the payload
+        data = {'password': password}
+
+        response = client_post_endpoint.post(f"/get_strength", json=data)
+        data = json.loads(response.data.decode())
+
+        assert response.status_code == 200
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'div_by_zero_post',
+        [
+            '',
+        ]
+    )
+    def test_api_divbyzeror_error(self, client_post_endpoint, div_by_zero_post):
+        # disambiguate
+        password = div_by_zero_post  # flake8 will say this is bad; ignore that!
+
+        # assemble the payload
+        data = {'password': password}
+
+        with pytest.raises(ZeroDivisionError):
+            client_post_endpoint.post(f"/get_strength", json=data)
+
+    @pytest.mark.parametrize(
+        'type_error_post',
+        [
+            1,
+            [1,2],
+            (1, 2),
+            {1:2}
+        ], ids=[
+            'int',
+            'list',
+            'tuple',
+            'dict'
+        ]
+    )
+    def test_api_type_error(self, client_post_endpoint, type_error_post):
+        # disambiguate
+        password = type_error_post  # flake8 will say this is bad; ignore that!
+
+        # assemble the payload
+        data = {'password': password}
+
+        with pytest.raises(TypeError):
+            client_post_endpoint.post(f"/get_strength", json=data)
---
+++ suny485/tests/hw15/test_api_as_live.py
@@ -0,0 +1,305 @@
+import json
+import pytest
+import requests
+from requests.exceptions import MissingSchema
+
+from suny485.projects.hw15.api import app
+
+"""
+testable things for API GET endpoint live testing
++ input
+    + key
+        + string
+            + must be 'password'
+            + else returns 500
+    + value
+        + string
+            + any string
+            + length of string
++ logic
+    + computes strength for good key/value pair
+    + http truncates url at `#`
+    + http truncates url at `&`
+    + empty value string throws DivByZero
++ output
+    + json payload
+        + key/value
+            + "password"
+                original string
+            + "strength"
+                either "good" or "bad"
+
+testable things for API POST endpoint live testing
++ input
+    + key
+        + string
+            + must be 'password'
+            + else returns 400 with payload
+                {'error': "Missing key 'password' in the JSON payload"}
+    + value
+        + string
+            + any string
+            + length of string
+            + empty string returns 500
+        + any other type returns 500
++ output
+    + json payload
+        + key/value
+            + "password"
+                original string
+            + "strength"
+                either "good" or "bad"
+
+"""
+
+class TestApiGetThroughHttp(object):
+
+    base = 'http://127.0.0.1:5000/get_strength'
+
+    @pytest.mark.parametrize(
+        'good_strength_get',
+        [
+            '_@4-%@o_rds',
+            '$$$$$$$ord',
+        ], ids=[
+            '_@4-%@o_rds',
+            '$$$$$$$ord',
+        ]
+    )
+    def test_api_good_password(self, good_strength_get):
+        # disambiguate
+        password = good_strength_get  # flake8 will say this is bad; ignore that!
+        expected_strength = 'good'
+
+        # assemble the query params for requests; requests will create full url,
+        # which will look like "http://127.0.0.1:5000/get_strength?password=<password>"
+        params = {'password': password}
+
+        # make the live request, check that the server accepted it
+        res = requests.get(self.base, params=params)
+        assert res.status_code == 200
+
+        # get the response payload and examine it
+        data = res.json()
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_strength_get',
+        [
+            '_@4-%o_rds',
+            '$password',
+            # 'x' * 100,
+            # 'x' * 1000,
+            # 'x' * 10000,
+        ], ids=[
+            '_@4-%o_rds',
+            '$password',
+            # '100chars',
+            # '1000chars',
+            # '10000chars',
+        ]
+    )
+    def test_api_bad_password(self, bad_strength_get):
+        # disambiguate
+        password = bad_strength_get  # flake8 will say this is bad; ignore that!
+        expected_strength = 'bad'
+
+        # assemble the query params for requests; requests will create full url,
+        # which will look like "http://127.0.0.1:5000/get_strength?password=<password>"
+        params = {'password': password}
+
+        # make the live request, check that the server accepted it
+        res = requests.get(self.base, params=params)
+        assert res.status_code == 200
+
+        # get the response payload and examine it
+        data = res.json()
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_key_get',
+        [
+            'Password',
+            '$password',
+            'password '
+        ])
+    def test_api_bad_key(self, bad_key_get):
+        """ Keys that trigger a server 500 """
+        password = 'foo'
+
+        # assemble the query params for requests; requests will create full url,
+        # which will look like "http://127.0.0.1:5000/get_strength?password=<password>"
+        params = {bad_key_get: password}
+
+        # make the live request, check that the server accepted it
+        res = requests.get(self.base, params=params)
+        assert res.status_code == 500
+
+
+    @pytest.mark.parametrize(
+        'not_schema_errors_get',
+        [
+            'pass#word',
+            'pa??word',
+            'pa&&word',
+        ]
+    )
+    def test_url_not_schema_error(self, not_schema_errors_get):
+        """ If unescaped in query string, these would cause string truncation """
+        # disambiguate
+        password = not_schema_errors_get  # flake8 will say this is bad; ignore that!
+
+        # assemble the query params for requests; requests will create full url,
+        # which will look like "http://127.0.0.1:5000/get_strength?password=<password>"
+        params = {'password': password}
+
+        # make the live request, check that the server accepted it
+        res = requests.get(self.base, params=params)
+        assert res.status_code == 200
+
+    @pytest.mark.parametrize(
+        'div_by_zero_get',
+        [
+            '',
+        ]
+    )
+    def test_api_divbyzeror_error(self, div_by_zero_get):
+        # disambiguate
+        password = div_by_zero_get  # flake8 will say this is bad; ignore that!
+
+        # assemble the payload
+        params = {'password': password}
+
+        res = requests.get(f"{self.base}", params=params)
+        assert res.status_code == 500
+
+
+class TestApiPostGetThroughHttp(object):
+
+    base = 'http://127.0.0.1:5000/get_strength?'
+
+    @pytest.mark.parametrize(
+        'good_strength_post',
+        [
+            '_@4-%@o_rds',
+            '$$$$$$$ord',
+        ], ids=[
+            '_@4-%@o_rds',
+            '$$$$$$$ord',
+        ]
+    )
+    def test_api_good_password(self, good_strength_post):
+        # disambiguate
+        password = good_strength_post  # flake8 will say this is bad; ignore that!
+        expected_strength = 'good'
+
+        # assemble the payload
+        data = {'password': password}
+
+        res = requests.post(f"{self.base}", json=data)
+        assert res.status_code == 200
+
+        data = res.json()
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_strength_post',
+        [
+            '_@4-%o_rds',
+            '$password',
+            # 'x' * 100,
+            # 'x' * 1000,
+            # 'x' * 10000,
+        ], ids=[
+            '_@4-%o_rds',
+            '$password',
+            # '100chars',
+            # '1000chars',
+            # '10000chars',
+        ]
+    )
+    def test_api_bad_password(self, bad_strength_post):
+        # disambiguate
+        password = bad_strength_post  # flake8 will say this is bad; ignore that!
+        expected_strength = 'bad'
+
+        # assemble the payload
+        data = {'password': password}
+
+        res = requests.post(f"{self.base}", json=data)
+        assert res.status_code == 200
+
+        # get the response payload and examine it
+        data = res.json()
+        print(f"data: {data}")
+        assert data == {'password': password, 'strength': expected_strength}
+
+    @pytest.mark.parametrize(
+        'bad_key_post',
+        [
+            'Password',
+            '$password',
+            'password '
+        ])
+    def test_api_bad_key(self, bad_key_post):
+        """ Keys that trigger a server 400 """
+        key = bad_key_post
+        password = 'foo'
+
+        # assemble the payload
+        data = {key: password}
+
+        res = requests.post(f"{self.base}", json=data)
+        assert res.status_code == 400
+
+        # get the response payload and examine it
+        data = res.json()
+        print(f"data: {data}")
+        assert data == {'error': "Missing key 'password' in the JSON payload"}
+
+    @pytest.mark.parametrize(
+        'type_error_post',
+        [
+            1,
+            [1,2],
+            (1, 2),
+            {1:2}
+        ], ids=[
+            'int',
+            'list',
+            'tuple',
+            'dict'
+        ]
+    )
+    def test_api_type_error(self, type_error_post):
+        # disambiguate
+        password = type_error_post  # flake8 will say this is bad; ignore that!
+        expected_strength = 'bad'
+
+        # assemble the payload
+        data = {'password': password}
+
+        res = requests.post(f"{self.base}", json=data)
+        assert res.status_code == 500
+
+        # the server should show that this raises a TypeError
+
+    @pytest.mark.parametrize(
+        'div_by_zero_post',
+        [
+            '',
+        ]
+    )
+    def test_api_divbyzeror_error(self, div_by_zero_post):
+        # disambiguate
+        password = div_by_zero_post  # flake8 will say this is bad; ignore that!
+
+        # assemble the payload
+        data = {'password': password}
+
+        res = requests.post(f"{self.base}", json=data)
+        assert res.status_code == 500
+
