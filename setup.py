3f5d65c95f8a20405dede095efabb2aac88ed1d3
derek <philosophe@gmail.com>
2023-11-09 14:21:17 -0500

    add github workflow
    .github/workflows/push.yml
    + tweaked instructions
    
    requirements.txt
    + dropped version for flask
    
    setup.py
    + added setup config for installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='suny485',
+   version='1.0',
+   description='code work for the class CSC 485',
+   author='Derek Sisson',
+   packages=['suny485'],
+)
